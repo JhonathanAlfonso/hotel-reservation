157199742021be44d063eb4c7d2415f8a1e41e5d
Jhonathan Salazar <salajhon69@gmail.com>
2021-08-18 07:48:22 -0500

    feat: get and store reservation end point implemented

--- src/main/java/com/linkedinlearning/reactivespring/controller/ReservationResource.java
+++ src/main/java/com/linkedinlearning/reactivespring/controller/ReservationResource.java
@@ -1,6 +1,8 @@
 package com.linkedinlearning.reactivespring.controller;
 
 import com.linkedinlearning.reactivespring.model.Reservation;
+import com.linkedinlearning.reactivespring.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -12,17 +14,21 @@ public class ReservationResource {
 
     public static final String ROOM_VI_RESERVATION = "/room/v1/reservation/";
 
-    @GetMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> getReservationById(@PathVariable String roomId) {
+    private final ReservationService reservationService;
 
-        //reservationService.getReservation(roomId);
+    @Autowired
+    public ReservationResource(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
-        return Mono.just("{}");
+    @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Reservation> getReservationById(@PathVariable String id) {
+        return reservationService.getReservation(id);
     }
 
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> createReservation(@RequestBody Mono<Reservation> reservation) {
-        return Mono.just("{}");
+    public Mono<Reservation> createReservation(@RequestBody Mono<Reservation> reservation) {
+        return reservationService.createReservation(reservation);
     }
 
     @PutMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
